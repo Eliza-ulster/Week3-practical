e9c8d78b538e9d5685443833613cb385c940ed6a
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-08 10:19:01 +0100

    added a contructor

--- Library/Program.cs
+++ Library/Program.cs
@@ -3,39 +3,33 @@
     string Title;
     string Author;
     string ISBN;
-    int NoOfPages;
+ 
+
+    public Book(string title, string author, string iSBN)
+    {
+        Title = title;
+        Author = author;
+        ISBN = iSBN;
+    }
 
     void DisplayInfo()
 
     {
         Console.WriteLine("Book Inforamation");
         Console.WriteLine("-----------------");
-        Console.WriteLine($"Book Title;{Title}");
-        Console.WriteLine($"Book Author:{Author}");
-        Console.WriteLine($"Book ISBN:{ISBN}");
-        Console.WriteLine($"No of pages : {NoOfPages}");
+        Console.WriteLine($"Book Title : {Title}");
+        Console.WriteLine($"Book Author : {Author}");
+        Console.WriteLine($"Book ISBN : {ISBN}");
+        Console.WriteLine();
+    
     }
 
     static void Main(string[] args)
     {
-        // Create a new instance of the book class
-        Book book = new Book();
 
-        book.Title = " C# for beginners";
-        book.Author = "Bill Gates";
-        book.ISBN = "123456699";
-        book.NoOfPages = 200;
+        Book book = new Book("C# for beginners", "Bill Gates", "123567800");
 
-        Book book2 = new Book();
-
-        book2.Title = "C# Methods and Classes";
-        book2.Author = " Microsoft";
-        book2.ISBN = "23556189";
-        book2.NoOfPages = 300;
-        
-        // output book information to the Console
         book.DisplayInfo();
-        book2.DisplayInfo();
 
 
 
