b96e265a3d42edb3ee2c132f75ecc15596b94c99
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-08 10:11:16 +0100

    pages

--- Library/Program.cs
+++ Library/Program.cs
@@ -3,6 +3,7 @@
     string Title;
     string Author;
     string ISBN;
+    int NoOfPages;
 
     void DisplayInfo()
 
@@ -12,6 +13,7 @@
         Console.WriteLine($"Book Title;{Title}");
         Console.WriteLine($"Book Author:{Author}");
         Console.WriteLine($"Book ISBN:{ISBN}");
+        Console.WriteLine($"No of pages : {NoOfPages}");
     }
 
     static void Main(string[] args)
@@ -22,12 +24,14 @@
         book.Title = " C# for beginners";
         book.Author = "Bill Gates";
         book.ISBN = "123456699";
+        book.NoOfPages = 200;
 
         Book book2 = new Book();
 
         book2.Title = "C# Methods and Classes";
         book2.Author = " Microsoft";
         book2.ISBN = "23556189";
+        book2.NoOfPages = 300;
         
         // output book information to the Console
         book.DisplayInfo();
